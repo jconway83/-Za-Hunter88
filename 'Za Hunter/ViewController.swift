21b96b48d3b8a5c72324800a0546e52d2bdd2eb2
Ella <ewickstrom@bsd220.org>
2020-03-03 10:59:53 -0600

    put nearby pizza places on the map

--- 'Za Hunter/ViewController.swift
+++ 'Za Hunter/ViewController.swift
@@ -42,10 +42,29 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         search.start { (response, error) in
             if let response = response {
                 for mapItem in response.mapItems {
-                    print(mapItem.name!)
-                }
+                     let annotation = MKPointAnnotation()
+                     annotation.coordinate = mapItem.placemark.coordinate
+                     annotation.title = mapItem.name
+                     self.mapView.addAnnotation(annotation)
+                 }
             }
         }
     }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+           return nil
+        }
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: "pin")
+        if pinView == nil {
+           pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pinView")
+           pinView?.canShowCallout = true
+           pinView?.rightCalloutAccessoryView = UIButton(type: .infoLight)
+        } else {
+           pinView?.annotation = annotation
+        }
+        return pinView
+    }
+    
 }
 
